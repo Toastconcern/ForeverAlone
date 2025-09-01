12296673bc40b246781070a77e47323b8c632df0
Seventh Grand Dad <seventhgranddad@national.shitposting.agency>
2025-09-01 00:26:42 -0400

    Rewrite entire mod.

--- ForeverAlone/Plugin.cs
+++ ForeverAlone/Plugin.cs
@@ -1,29 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using BepInEx;
-using UnityEngine;
+using GorillaNetworking;
+using HarmonyLib;
+using static ForeverAlone.HarmonyPatches;
 
 namespace ForeverAlone
 {
-	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-	public class Plugin : BaseUnityPlugin
-	{
-        GameObject friends;
-        GameObject joins;
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Start() => ApplyHarmonyPatches();
 
-        void Start()
-		{
-            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+        // __originalMethod needed so that the method signatures always match.
+        // If it's not there, things will break.
+        public static bool StubPatch(MethodBase __originalMethod) => false;
+
+        public static void ApplyPatches(Harmony harmony)
+        {
+            var targetMethods = new List<MethodInfo>
+            {
+                GetMethodSafe(typeof(GorillaNetworkJoinTrigger), "OnBoxTriggered"),
+                GetMethodSafe(typeof(GorillaNetworkLeaveRoomTrigger), "OnBoxTriggered"),
+                GetMethodSafe(typeof(GorillaNetworkLeaveTutorialTrigger), "OnBoxTriggered"),
+                GetMethodSafe(typeof(PhotonNetworkController), "AttemptToJoinSpecificRoomAsync")
+            };
+            var prefix = typeof(Plugin).GetMethod("StubPatch",
+                BindingFlags.Static | BindingFlags.Public);
+
+            foreach (var method in targetMethods)
+            {
+                harmony.Patch(original: method, prefix: new HarmonyMethod(prefix));
+                Console.WriteLine($"patched {method.DeclaringType.FullName}.{method.Name}");
+            }
         }
-
-		void OnGameInitialized()
-		{
-            NetworkSystem.Instance.OnMultiplayerStarted += OnJoin;
-            friends = GameObject.Find("FriendsJoining_Prefab");
-            joins = GameObject.Find("JoinRoomTriggers_Prefab");
-            friends.Destroy();
-            joins.Destroy();
-        }
-
-		public void OnJoin() => NetworkSystem.Instance.ReturnToSinglePlayer();
     }
 }
