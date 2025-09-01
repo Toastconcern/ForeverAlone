4e77a30a833379de8831c5f6340f5ace5f8d59a7
ToastConcern <94580243+Toastconcern@users.noreply.github.com>
2025-08-31 20:28:14 -0500

    Initial commit

---
+++ ForeverAlone/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+
+namespace ForeverAlone
+{
+	/// <summary>
+	/// This is your mod's main class.
+	/// </summary>
+
+
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+        GameObject friends;
+        GameObject joins;
+        void Start()
+		{
+            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+        }
+
+		void OnGameInitialized()
+		{
+            NetworkSystem.Instance.OnMultiplayerStarted += OnJoin;
+            friends = GameObject.Find("FriendsJoining_Prefab");
+            joins = GameObject.Find("JoinRoomTriggers_Prefab");
+            friends.Destroy();
+            joins.Destroy();
+        }
+
+		public void OnJoin() => NetworkSystem.Instance.ReturnToSinglePlayer();
+    }
+}
---
+++ ForeverAlone/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace ForeverAlone
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.toastconcern.gorillatag.foreveralone";
+		public const string Name = "ForeverAlone";
+		public const string Version = "1.0.0";
+	}
+}
